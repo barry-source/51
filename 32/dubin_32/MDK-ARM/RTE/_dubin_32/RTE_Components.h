026abb480a07387a05fc9b1595f5c70ff0fb3c49
tsc <s787753577@qq.com>
2023-09-17 17:01:52 +0800

    dusbint

---
+++ 32/dubin_32/MDK-ARM/RTE/_dubin_32/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dubin_32' 
+ * Target:  'dubin_32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
